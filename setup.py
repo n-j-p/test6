e2fde3477bb204d6b85ef6f0f94886e1cea3a2ec
n-j-p <noreply@github.com>
2023-02-09 08:37:11 +1100

    added setup.py. "pip install -e ." works.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='example_package_njp',
+    version='dev',
+
+    url='https://github.com/n-j-p/test6',
+    author='n-j-p',
+
+    py_modules=['example_package_njp'],
+)
